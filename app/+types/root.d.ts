73a85d70391df8d89d336b35f721e347976344eb
furqan5921 <furqanali5921@gmail.com>
2026-01-27 02:51:57 +0530

    fixed text alignemnt and backspace

---
+++ app/+types/root.d.ts
@@ -0,0 +1,8 @@
+// Generated React Router types for root route
+import type { LinksFunction, LoaderFunctionArgs } from "@react-router/node";
+
+export interface Route {
+  LinksFunction?: LinksFunction;
+  LoaderFunction?: (args: LoaderFunctionArgs) => any;
+}
+
